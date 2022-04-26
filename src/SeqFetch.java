a5bf3e2e2b54d517ed8998e2b7d778afe9d23e2e
Johann Kwon <johannkwoncs@gmail.com>
2022-04-26 01:23:52 -0700

    Uploaded edits

--- src/SeqFetch.java
+++ src/SeqFetch.java
@@ -6,13 +6,12 @@ import java.util.Map;
 public class SeqFetch {
 
     private HashMap<String, String> sequences;
-    private HashMap<String, String> alignedSequences;
 
     public SeqFetch() {
         sequences = new HashMap<String, String>();
-        alignedSequences = new HashMap<String, String>();
     }
 
+    // "Strains" sequences from a file by some specified name and puts results into a new file
     public void strain(String genus, File f) throws Exception {
         BufferedReader b = new BufferedReader(new FileReader(f));
         String buffer = "";
@@ -65,14 +64,10 @@ public class SeqFetch {
         System.out.println(count);
     }
 
+    // Removes duplicates from FASTA file and creates a new FASTA file without duplicates. Also adds to the
     public void removeDuplicates(String genus, File f, int limit) throws Exception {
         BufferedReader b = new BufferedReader(new FileReader(f));
-
         String buffer = "";
-        String temp = "";
-        String nextLine = "";
-        boolean organismSeq = false;
-
         File nf = new File("data\\" + genus + "_unique_" + limit + ".fa");
 
         FileWriter fw = new FileWriter(nf);
@@ -89,19 +84,15 @@ public class SeqFetch {
         int count = 0;
 
         while(!cont) {
-            /*
-            if(count == limit) {
-                break;
-            }*/
             buffer = b.readLine();
             def = buffer;
-            //System.out.println(def);
+            System.out.println(def);
             if(def == null) {
                 break;
             }
             buffer = b.readLine();
             sequence = buffer;
-            //System.out.println(sequence);
+            System.out.println(sequence);
             if(sequence == null) {
                 break;
             }
@@ -132,6 +123,7 @@ public class SeqFetch {
         System.out.println("number of sequences added to file: " + iterator);
     }
 
+    // Concatenating FASTA files back together
     public void concatenate(File first, File second, String firstGenus, String secondGenus, int firstSize, int secondSize) throws Exception {
         BufferedReader firstRead = new BufferedReader(new FileReader(first));
         BufferedReader secondRead = new BufferedReader(new FileReader(second));
@@ -139,7 +131,6 @@ public class SeqFetch {
         int sum = firstSize + secondSize;
 
         File nf = new File("data\\stitched_" + sum + "_" + firstGenus + "_" + secondGenus + ".fa");
-
         FileWriter fw = new FileWriter(nf);
         BufferedWriter br = new BufferedWriter(fw);
 
@@ -157,6 +148,7 @@ public class SeqFetch {
         br.close();
     }
 
+    // Splits an unaligned FASTA file into two files, one containing 80% of the sequences and the other containing 20%
     public void splitSeeds(File seeds, String name, int size) throws Exception {
         BufferedReader fileRead = new BufferedReader(new FileReader(seeds));
 
@@ -212,35 +204,71 @@ public class SeqFetch {
         brTwenty.close();
     }
 
+    // Identifies sequence by matching accession numbers with sequence deflines
+    public void identify(File numbers, String name) throws Exception {
+        BufferedReader fileRead = new BufferedReader(new FileReader(numbers));
+
+        File f = new File("data\\" + name + "_accno_names" + ".txt");
+        FileWriter fw = new FileWriter(f);
+        BufferedWriter br = new BufferedWriter(fw);
+
+        String buffer = "";
+        while((buffer = fileRead.readLine()) != null) {
+            for(String def : sequences.values()) {
+                if(def.indexOf(buffer.substring(0, 8)) != -1) {
+                    int organism = def.indexOf("organism=");
+                    br.write(def.substring(organism + 9));
+                    br.newLine();
+                } else {
+                    continue;
+                }
+            }
+        }
+        br.close();
+    }
+
 
     public static void main(String[] args) throws Exception {
         File readFile = new File("D:\\workspaces\\proj_123b\\data\\fungene_9.9.11_nifH_9724_unaligned_protein_seqs.fa");
         SeqFetch sf = new SeqFetch();
 
-/*
+        // Operations for each genus must be done individually
+
+        /*
+        // Rhizobium
         String nameR = "Rhizobium"; //Mesorhizobium
         File orgFileR = new File("D:\\workspaces\\proj_123b\\data\\" + nameR + ".fa");
         sf.strain(nameR, readFile);
-        sf.removeDuplicates(nameR, orgFileR, 50);*/
+        sf.removeDuplicates(nameR, orgFileR, 50);
+
+        File rUnique = new File("D:\\workspaces\\proj_123b\\data\\Rhizobium_unique_25.fa");
+        // sf.splitSeeds(rUnique, "Rhizobium", 25);
+
+        File rAccno = new File("D:\\workspaces\\proj_123b\\data\\rhizobium_accno.txt");
+        sf.identify(rAccno, "Rhizobium");*/
 
 
+        // Mesorhizobium
         String nameM = "Mesorhizobium";
         File orgFileM = new File("D:\\workspaces\\proj_123b\\data\\" + nameM + ".fa");
         sf.strain(nameM, readFile);
         sf.removeDuplicates(nameM, orgFileM, 50);
+
+        File mUnique = new File("D:\\workspaces\\proj_123b\\data\\Mesorhizobium_unique_25.fa");
+        // sf.splitSeeds(mUnique, "Mesorhizobium", 25);
+
+        File mAccno = new File("D:\\workspaces\\proj_123b\\data\\mesorhizobium_accno.txt");
+        sf.identify(mAccno, "Mesorhizobium");
+
 
         System.out.println("total number of unique sequences: " + sf.sequences.keySet().size());
         for(Map.Entry<String, String> seq : sf.sequences.entrySet()) {
             System.out.println(seq.getValue());
         }
 
-
-        File rUnique = new File("D:\\workspaces\\proj_123b\\data\\Rhizobium_unique_25.fa");
-        File mUnique = new File("D:\\workspaces\\proj_123b\\data\\Mesorhizobium_unique_25.fa");
         // sf.concatenate(rUnique, mUnique, "Rhizobium", "Mesorhizobium", 25, 25);
 
-        //sf.splitSeeds(rUnique, "Rhizobium", 25);
-        sf.splitSeeds(mUnique, "Mesorhizobium", 25);
+
     }
 
 }
